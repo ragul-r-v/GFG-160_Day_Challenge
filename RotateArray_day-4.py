52c98dadc6ce5bdc81d8213b8cae67f080cda05e
Ragul R V <rvragulvenkatesan@gmail.com>
2026-02-08 23:58:57 +0530

    Day-4: modified code

--- RotateArray_day-4.py
+++ RotateArray_day-4.py
@@ -1,18 +1,26 @@
-def reverse(arr, start, end):
-    while start < end:
-        arr[start], arr[end] = arr[end], arr[start]
-        start += 1
-        end -= 1
 
-def leftRotate(arr, k):
-    n = len(arr)
-    k = k % n
 
-    reverse(arr, 0, k-1)
-    reverse(arr, k, n-1)
-    reverse(arr, 0, n-1)
+class Solution:
+    def rotateArr(Self,arr,d):
+        n = len(arr)
+        d%=n
+        reverse(arr, 0, d-1)
+        reverse(arr, d, n-1)            
+        reverse(arr, 0, n-1)
 
-    return arr
+def reverse(arr,start,end):
+    while start<end:
+        arr[start],arr[end] = arr[end],arr[start]
+        start+=1
+        end-=1
 
-arr = [1,2,3,4,5]
-print(leftRotate(arr, 2))
+## for vs code run
+
+
+if __name__ == "__main__":
+    arr = [1,2,3,4,5]
+    d = 2
+    obj = Solution()
+    obj.rotateArr(arr,d)    
+    print(*arr)
+    
