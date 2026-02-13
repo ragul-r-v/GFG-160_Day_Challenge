855fe9eaaf9125bb7f21f4ab9d5d2a56596927d8
Ragul R V <rvragulvenkatesan@gmail.com>
2026-02-13 23:48:53 +0530

    Day-8 Adds Snippet

--- StockBuyandSell(withonelimit)_day-8.py
+++ StockBuyandSell(withonelimit)_day-8.py
@@ -14,3 +14,22 @@ if __name__ == "__main__":
     prices = [7,10,1,3,6,9,2]
     obj = Solution()
     print(obj.maximumProfit(prices))
+
+
+
+
+class Solution:
+    def maximumProfit(self, prices):
+        minSoFar = prices[0]
+        profit = 0
+
+        for price in prices:
+            minSoFar = min(minSoFar, price)
+            profit = max(profit, price - minSoFar)
+
+        return profit
+
+
+if __name__ == "__main__":
+    prices = [7,10,1,3,6,9,2]
+    print(Solution().maximumProfit(prices))
