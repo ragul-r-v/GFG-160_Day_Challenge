156328e2f9b72175fc4476d9091c746ab890ced8
Ragul R V <rvragulvenkatesan@gmail.com>
2026-02-17 23:27:13 +0530

    Day-9 Added Details for the Code

--- MinimizetheHeights_day-9.py
+++ MinimizetheHeights_day-9.py
@@ -2,18 +2,18 @@ class Solution:
     def getMinDiff(self, arr, k):
         # code here
         n = len(arr)
-        arr.sort()
-        res = arr[n-1]-arr[0]
+        arr.sort()                          # Sort the array to make it easier to find the minimum and maximum heights after modification
+        res = arr[n-1]-arr[0]               # Initialize the result as the difference between the maximum and minimum heights without any modification
         
-        for i in range(1,len(arr)):
-            if arr[i]-k<0:
-                continue
-            minH=min(arr[0]+k,arr[i]-k)
-            maxH=max(arr[i-1]+k,arr[n-1]-k)
-            res=min(res,maxH-minH)
-        return res
-if __name__ == "__main__":
-    arr = [1, 5, 8, 10]
-    k = 2
-    ans = Solution().getMinDiff(arr, k)
-    print(ans)
+        for i in range(1,len(arr)):         # Iterate through the sorted array starting from the second element
+            if arr[i]-k<0:                  # If the current height minus k is negative, we cannot reduce it further, so we skip this iteration ,because we cannot have negative heights
+                continue                    # Calculate the minimum and maximum heights after modification
+            minH=min(arr[0]+k,arr[i]-k)     # The minimum height can be either the smallest height plus k or the current height minus k
+            maxH=max(arr[i-1]+k,arr[n-1]-k) # The maximum height can be either the previous height plus k or the largest height minus k
+            res=min(res,maxH-minH)          # Update the result with the minimum difference found so far
+        return res                          # Return the minimum difference between the maximum and minimum heights after modification
+if __name__ == "__main__":                  # Example usage
+    arr = [1, 5, 8, 10]                     # The array of heights
+    k = 2                                       # The value by which we can increase or decrease the heights
+    ans = Solution().getMinDiff(arr, k)     # Calculate the minimum difference between the maximum and minimum heights after modification
+    print(ans)                          # Print the result
